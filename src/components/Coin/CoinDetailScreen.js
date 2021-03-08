4330cb8d23f98f4eabe62c7356b4e371202af39a
Javier Soto <javislop@gmail.com>
2021-03-08 12:48:08 -0600

    get coin img and add styles to coin detail

--- src/components/Coin/CoinDetailScreen.js
+++ src/components/Coin/CoinDetailScreen.js
@@ -1,17 +1,53 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Colors } from '../../res/colors';
 
-const CoinDetailScreen = ({ coin }) => {
+const CoinDetailScreen = ({ route, navigation }) => {
+  const { coin } = route.params;
 
   useEffect(() => {
-    console.log('coin :>> ', coin);
+    navigation.setOptions({ title: coin.symbol })
   }, [])
 
+  const getSymbolIcon = () => {
+    const nameId = coin.nameid;
+
+    if (nameId) {
+      return `https://c1.coinlore.com/img/25x25/${nameId}.png`;
+    }
+  }
+
   return (
-    <View>
-      <Text>CoinDetailScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.subHeader}>
+        <Image style={styles.iconImg} source={{ uri: getSymbolIcon() }}/>
+        <Text style={styles.titleText}>{coin.name}</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.charade,
+  },
+  subHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    padding: 16,
+  },
+  iconImg: {
+    width: 25,
+    height: 25,
+  },
+  titleText: {
+    marginLeft: 10,
+    color: Colors.white,
+    fontWeight: 'bold',
+    fontSize: 18,
+  }
+});
 
 export default CoinDetailScreen;
